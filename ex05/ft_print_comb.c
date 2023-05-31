4e5b857a824e37458ff257664783c492ff81f645
Joao Vitor Rodrigues De Oliveira <jovitor2@c2r7p4.42sp.org.br>
2023-05-31 17:10:09 -0300

    Exercicio 05 - Correção Virgula Final

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -6,11 +6,18 @@
 /*   By: jovitor2 <jovitor2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 18:08:01 by jovitor2          #+#    #+#             */
-/*   Updated: 2023/05/29 19:43:50 by jovitor2         ###   ########.fr       */
+/*   Updated: 2023/05/31 17:08:33 by jovitor2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+void	ft_putchar(int i, int j, int l)
+{
+	write(1, &i, 1);
+	write(1, &j, 1);
+	write(1, &l, 1);
+}
 
 void	ft_print_comb(void)
 {
@@ -29,11 +36,10 @@ void	ft_print_comb(void)
 			l = j + 1;
 			while ('9' >= l)
 			{
-				write (1, &i, 1);
-				write (1, &j, 1);
-				write (1, &l, 1);
-				write (1, ", ", 2);
+				ft_putchar(i, j, l);
 				l++;
+				if (i < '7')
+					write(1, ", ", 2);
 			}
 			j++;
 		}
