0282f7e55d61bce424aa00c89a9da5e2efee736e
Joao Rodrigues <jovitor2@student.42sp.org.br>
2023-06-01 11:50:54 -0300

    Correção Norminette ex08

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -6,10 +6,10 @@
 /*   By: jovitor2 <jovitor2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 11:46:54 by jovitor2          #+#    #+#             */
-/*   Updated: 2023/06/01 11:47:36 by jovitor2         ###   ########.fr       */
+/*   Updated: 2023/06/01 11:50:34 by jovitor2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_print_combn(int n){ 
-	
+void	ft_print_combn(int n)
+{
 }
